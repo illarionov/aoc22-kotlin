4ef5eb5aef3f6721f476144a1fa4e512a84cea5a
Alexey Illarionov <alexey@0xdc.ru>
2022-12-05 13:04:47 +0300

    Day 01
    Squashed commit of the following:
    
    commit f8af67e282667a3c5b66b7a7540d05312fe27ca1
    Author: Alexey Illarionov <alexey@0xdc.ru>
    Date:   Thu Dec 1 14:44:09 2022 +0300
    
        Day 1 dirty solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,47 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var current = 0
+        var max: Int = -1
+        input.forEach {s: String ->
+            when {
+                s.isEmpty() -> {
+                    max = maxOf(max, current)
+                    current = 0
+                }
+                else -> {
+                    current += s.toInt()
+                }
+            }
+        }
+        max = maxOf(max, current)
+        return max
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var current = 0
+        val max = intArrayOf(-1, -1, -1, -1)
+        input.forEach {s: String ->
+            when {
+                s.isEmpty() -> {
+                    max[3] = current
+                    max.sortDescending()
+                    max[3] = -1
+                    current = 0
+                }
+                else -> {
+                    current += s.toInt()
+                }
+            }
+        }
+        max[3] = current
+        max.sortDescending()
+        max[3] = 0
+        return max.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
