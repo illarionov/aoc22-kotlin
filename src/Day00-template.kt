947c05a52f441c9e460a9fb9c82e823ec754bd90
Alexey Illarionov <alexey@0xdc.ru>
2022-12-17 17:54:09 +0300

    Update template

--- src/Day00-template.kt
+++ src/Day00-template.kt
@@ -1,17 +1,32 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day00_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day00")
-    println(part1(input))
-    println(part2(input))
+
+    part1(testInput).also {
+        println("Part 1, test input: $it")
+        check(it == 1)
+    }
+
+    part1(input).also {
+        println("Part 1, real input: $it")
+        //check(it == 1)
+    }
+
+    part2(testInput).also {
+        println("Part 2, test input: $it")
+        check(it == 1)
+    }
+
+    part2(input).also {
+        println("Part 2, real input: $it")
+        //check(it == 1)
+    }
+}
+
+private fun part1(input: List<String>): Int {
+    return input.size
+}
+
+private fun part2(input: List<String>): Int {
+    return input.size
 }
