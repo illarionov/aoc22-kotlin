dfac703caa8f9fc5c632b78c8fbd4214b627fde1
Alexey Illarionov <alexey@0xdc.ru>
2022-12-20 20:21:31 +0300

    Day20: Circular doubly linked list approach

--- src/Day20.kt
+++ src/Day20.kt
@@ -1,88 +1,111 @@
-import java.util.*
 import kotlin.math.abs
 
 fun main() {
-    val testInput = readInput("Day20_test")
-        .map(String::toLong)
-    val input = readInput("Day20")
-        .map(String::toLong)
+    val testInput = readInput("Day20_test").map(String::toLong)
+    val input = readInput("Day20").map(String::toLong)
 
-    part1(testInput).also {
+    part1v2(testInput).also {
         println("Part 1, test input: $it")
         check(it == 3L)
     }
 
-    part1(input).also {
-        println("Part 1, real input: $it")
+    part1v2(input).also {
+        println("Part 1 1, real input: $it")
         check(it == 4914L)
     }
 
-    part2(testInput).also {
-        println("Part 2, test input: $it")
+    part2v2(testInput).also {
+        println("Part 2 2, test input: $it")
         check(it == 1623178306L)
     }
 
-    part2(input).also {
-        println("Part 2, real input: $it")
+    part2v2(input).also {
+        println("Part 2 2, real input: $it")
         check(it == 7973051839072)
     }
 }
 
-private data class V(
-    val value: Long,
-    val originalPosition: Int
-) {
-    override fun toString(): String {
-        return "$value($originalPosition)"
-    }
+private fun part1v2(input: List<Long>): Long {
+    val mixer = Mixer(input)
+    mixer.mix()
+    return listOf(1000, 2000, 3000).sumOf(mixer::getValueFromPositionOfZero)
 }
 
-private fun part1(input: List<Long>): Long {
-    val mixedList: MutableList<V> = LinkedList(input.mapIndexed { index, i -> V(i, index) })
-    mixedList.mix(input)
-
-    val indexOfZero = mixedList.indexOfFirst { v -> v.value == 0L }
-    return mixedList[(1000 + indexOfZero) % input.size].value +
-            mixedList[(2000 + indexOfZero) % input.size].value +
-            mixedList[(3000 + indexOfZero) % input.size].value
-}
-
-private fun part2(input: List<Long>): Long {
+private fun part2v2(input: List<Long>): Long {
     val input2 = input.map { i -> i * 811589153L }
-    val mixedList: MutableList<V> = LinkedList(input2.mapIndexed { index, i -> V(i, index) })
-
-    repeat(10) {
-        mixedList.mix(input2)
-    }
-    val indexOfZero = mixedList.indexOfFirst { v -> v.value == 0L }
-    return mixedList[(1000 + indexOfZero) % input2.size].value +
-            mixedList[(2000 + indexOfZero) % input2.size].value +
-            mixedList[(3000 + indexOfZero) % input2.size].value
+    val mixer = Mixer(input2)
+    repeat(10) { mixer.mix() }
+    return listOf(1000, 2000, 3000).sumOf(mixer::getValueFromPositionOfZero)
 }
 
-private fun MutableList<V>.mix(input: List<Long>) {
-    val listSize = input.size
-    input.forEachIndexed { index, i ->
-        val oldPosition = indexOfFirst { v -> v.originalPosition == index }
-        val repeatCnt = (abs(i) % (listSize - 1))
-        if (i > 0) {
-            val newPosition = (oldPosition + repeatCnt) % listSize
-            moveValue(oldPosition, newPosition.toInt())
-        } else if (i < 0) {
-            val newPosition = (oldPosition + listSize - repeatCnt - 1) % listSize
-            moveValue(oldPosition, newPosition.toInt())
+private class NumberNode(
+    val value: Long
+) {
+    var pred: NumberNode? = null /* by Delegates.notNull() */
+    var next: NumberNode? = null /* by Delegates.notNull() */
+}
+
+private class Mixer(input: List<Long>) {
+    val nodes = run {
+        val inputNodes: List<NumberNode> = input.map { i -> NumberNode(i) }
+        inputNodes.zipWithNext().forEach { (pred, next) ->
+            pred.next = next
+            next.pred = pred
+        }
+        inputNodes.last().next = inputNodes.first()
+        inputNodes.first().pred = inputNodes.last()
+        inputNodes
+    }
+
+    val zeroNode = nodes.first { n -> n.value == 0L }
+
+    private val size: Int = nodes.size
+
+    fun mix() {
+        nodes.forEach { currentNode ->
+            val nodesTraverse = (abs(currentNode.value) % (size - 1)).toInt()
+            if (nodesTraverse == 0 || currentNode.value == 0L) return@forEach
+
+            var newLeft: NumberNode = currentNode
+            var newRight: NumberNode = currentNode
+            if (currentNode.value > 0) {
+                repeat(nodesTraverse) {
+                    newLeft = newLeft.next!!
+                }
+                newRight = newLeft.next!!
+            } else {
+                repeat(nodesTraverse) {
+                    newRight = newRight.pred!!
+                }
+                newLeft = newRight.pred!!
+            }
+            val oldLeft = currentNode.pred
+            val oldRight = currentNode.next
+
+            newLeft.next = currentNode
+            currentNode.pred = newLeft
+            newRight.pred = currentNode
+            currentNode.next = newRight
+
+            oldLeft!!.next = oldRight
+            oldRight!!.pred = oldLeft
+        }
+    }
+
+    fun getValueFromPositionOfZero(position: Int): Long {
+        return nodesFromZero()
+            .drop(position % size)
+            .first()
+    }
+
+    fun nodesFromZero(): Sequence<Long> {
+        return sequence {
+            val root = zeroNode
+            var current = root
+            do {
+                yield(current.value)
+                current = current.next!!
+            } while (current != root)
         }
     }
 }
-
-private fun <T> MutableList<T>.moveValue(from: Int, to: Int) {
-    val oldValue = this[from]
-    if (to > from) {
-        add(to + 1, oldValue)
-        removeAt(from)
-    } else if (to < from) {
-        removeAt(from)
-        add(to + 1, oldValue)
-    }
-}
-
